5aa9d079a689fffa65f2b4d2f89ab2dc55c89632
Jordan Chong <jordanchong23@gmail.com>
2016-10-15 19:05:41 -0700

    Enable user signup with bcrypt and jwt

---
+++ controllers/authentication.js
@@ -0,0 +1,36 @@
+const jwt = require('jwt-simple');
+const User = require('../models/user');
+const secrets =  require('../secrets.js');
+
+function tokenForUser(user) {
+  const timestamp = new Date().getTime();
+  return jwt.encode({ sub: user.id, iat: timestamp }, secrets.secretString) // sub "subject" and iat "issued at time" and JWT web standards
+}
+
+exports.signup = function(req, res, next) {
+  const email = req.body.email;
+  const password = req.body.password;
+  // validate user provided data
+  if(!email || !password) {
+    return res.status(422).send({ error:  'you must provide an email and password'});
+  }
+  // See if a user with a given email exists
+  User.findOne( {email: email }, function(err, existingUser) {
+    if (err) { return next(err); }
+    // If a user with email does exist, return an error
+    if(existingUser) {
+      return res.status(422).send({ error: 'email is in use' }); // 422 - "unprocessable entity"
+    }
+    // if a user with that email does NOT exist create and save a new user record 
+    const user  = new User({
+      email: email,
+      password: password,
+    });
+    user.save(function(err) {
+      if(err) { return next(err); };
+      // respons to request indicating that the user was created
+      res.json({ token: tokenForUser(user) });
+    });
+  });
+
+}
---
+++ models/user.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt-nodejs');
+
+//Define our Model
+const userSchema = new Schema({
+  email: { type: String, unique: true, lowercase: true }, // *unique is case sensitive so lowercase is needed
+  password: String,
+});
+
+// On save hook, encrypt password. Before saving a model, run this callback
+userSchema.pre('save', function(next) {
+  // Get access to this user model
+  const user = this;
+
+  // generate salt then run callback
+  bcrypt.genSalt(10, function(err, salt) {
+    if(err) { return next(err); }
+
+    // hash (encrypt) our password using the salt
+    bcrypt.hash(user.password, salt, null, function(err, hash) {
+      if(err) { return next(err); }
+
+      // overwrite plain text password with encrypted password
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+// Create the model class
+const ModelClass = mongoose.model('user', userSchema);
+
+module.exports = ModelClass;
